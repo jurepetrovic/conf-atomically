9d5fdd50f6689157693b4faa72a5f3205a40e2f9
Jure Petrovic <jure@enerco.si>
2020-07-15 09:30:57 +0200

    removed console.log

--- source/index.ts
+++ source/index.ts
@@ -456,7 +456,6 @@ class Conf<T extends Record<string, any> = Record<string, unknown>> implements I
 				// Fix for https://github.com/sindresorhus/electron-store/issues/106
 				// Sometimes on Windows, we will get an EXDEV error when atomic writing
 				// (even though to the same directory), so we fall back to non atomic write
-				console.log(`CATCH::_write:${String(error.code)}`);
 				if (error.code === 'EXDEV' || error.code === 'EPERM') {
 					fs.writeFileSync(this.path, data);
 					return;
