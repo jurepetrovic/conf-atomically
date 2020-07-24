f4d3744d19f04fdbf9f7dffea6242baf544cd41b
Jure Petrovic <jure@enerco.si>
2020-07-24 10:51:35 +0200

    Increased write file timeout to 1 second.

--- source/index.ts
+++ source/index.ts
@@ -451,7 +451,8 @@ class Conf<T extends Record<string, any> = Record<string, unknown>> implements I
 			fs.writeFileSync(this.path, data);
 		} else {
 			try {
-				atomically.writeFileSync(this.path, data);
+				// add timeout for some windows problems ;)
+				atomically.writeFileSync(this.path, data, { timeout:1000 });
 			} catch (error) {
 				// Fix for https://github.com/sindresorhus/electron-store/issues/106
 				// Sometimes on Windows, we will get an EXDEV error when atomic writing
